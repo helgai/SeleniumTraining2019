385e0a28e8d4349f88f3b9dfa2b7230c80e174b6
HelgaI <ghelgai@gmail.com>
2019-03-03 15:08:49 +0300

    Task1

---
+++ csharp-example/csharp-example/BrowserTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class BrowserTests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void TestBrowserOpen()
+        {
+            driver.Url = "http://www.google.com/";
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
